810123d5f1598f4b7b0de69aa1b6a773718ce5bc
dprvulov <dejan.prv@gmail.com>
2020-02-07 20:28:17 +0100

    fetcher works

--- src/Components/Fetcher.js
+++ src/Components/Fetcher.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+// json-server --watch db.json --port 3004
 // https://www.wienerlinien.at/ogd_realtime/monitor?&rbl=1212&rbl=1345&rbl=5568&rbl=2910&rbl=4203&rbl=4212&rbl=46&rbl=18&rbl=1303&rbl=3701
 
 /**
@@ -12,26 +12,43 @@ const Fetcher = ({Haltestellen, boxData, handleBoxData}) => {
   const station = boxData.station;
   let [data, setData] = useState({ lines: [] });
 
+  // fetch data from API
   const fetchData = async (station) => {
     let RBL = await Haltestellen.filter(e => e.NAME === station)[0].RBL;
     let url = "https://www.wienerlinien.at/ogd_realtime/monitor?" 
     for (let i = 0; i < RBL.length; i++) {
       url = `${url}&rbl=${RBL[i]}`;
     }
-    try {
-      let response = await fetch(url, {mode: 'cors'});
-      let data = await response.json();
-    } 
-    finally {
-      let d = await fetch("https://my-json-server.typicode.com/DPDP93/qando-db/db");
-      return await d.json();
-    }
+
+    //let response = await fetch(url, {mode: 'cors'});
+    let response = await fetch("http://localhost:3004/data");
+    let data = await response.json();
+    return data; 
   }
 
+  // extract data from API response
+  const formatData = async (d) => {
+    // d.monitors[0].lines[0].departures.departure[0].departureTime.countdown
+    let data = { lines:[] };
+    d.monitors.map(e => {
+      let line = {};
+      line.name = e.lines[0].name;
+      line.towards = e.lines[0].towards;
+      line.departures = [ e.lines[0].departures.departure[0].departureTime.countdown, 
+                          e.lines[0].departures.departure[1].departureTime.countdown
+                        ];
+      data.lines = [...data.lines, line];
+    });
+    return data;
+  }
+
+  // when station changes, fetch and prepare new data
   useEffect(() => {
-    fetchData(station)  // here must go RBL
-      .then(x => {
-        data = setData(x);
+    fetchData(station) 
+      .then(newData => formatData(newData))
+      .then(data => {
+        console.log(data);
+        setData(data);
       });
   }, [station])
 
